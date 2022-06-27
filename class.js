0c22db66f7bbf426a6cff8da370021333725aef3
D Allingham <davida@baryonicmatter.net>
2022-06-27 15:22:27 +1000

    Added code and helper functions for finding the destination of addresses under an automorphism.

--- class.js
+++ class.js
@@ -43,7 +43,7 @@ class Vertex {
 */
 
  toString(){
-  if (this.address == null) return 'undefined';
+  if (this.address == undefined) return 'undefined';
   return this.address.toString();
  }
 
@@ -51,7 +51,7 @@ class Vertex {
   // label the vertex according to the 'label alphabet' of its graph, indexed by the entries in the
   // vertex's address; special case: address is the empty array: set the label to \u{d8} (ie. 'Ø')
   const ROOT_VERTEX_LABEL = "\u{d8}";
-  if (this.address!=null){
+  if (this.address != undefined){
    var thelabel = this.address.map(t=>this.graph.alphabet[t]).join("");
    return (thelabel.length==0?ROOT_VERTEX_LABEL:thelabel);
   }
@@ -77,8 +77,8 @@ class Edge {
   this.name = name;
   this.id = ++Edge.number_of_edges;
 
-  this.from = null;
-  this.to = null;
+  this.from = undefined;
+  this.to = undefined;
 
   this.is_directed = false;
  }
@@ -231,21 +231,27 @@ class Automorphism {
   this.name = name;
   this.valency = valency;
 
-  this.reference_address = null;
-  this.destination_address = null;
+  this.reference_address = undefined;
+  this.destination_address = undefined;
   this.local_actions = [];
   this.constant_local_action = false;
 
-  this.address_destinations = null;
+  this.address_destinations = [];
  }
 
  // in the following methods, 'address' is an array of integers
  set_reference_address(address){
   this.reference_address = address;
+  if (this.destination_address != undefined){
+   this.address_destinations[address] = this.destination_address;
+  }
  }
 
  set_destination_address(address){
   this.destination_address = address;
+  if (this.reference_address != undefined){
+   this.address_destinations[this.reference_address] = address;
+  }
  }
 
  test_permutation(perm){
@@ -284,21 +290,97 @@ class Automorphism {
   return (this.get_local_action_addresses().indexOf(address.toString())>-1?true:false);
  }
 
+ get_local_action_at_address(address){
+  // first check for constant local action: use the LA of the reference address
+  if (this.constant_local_action){
+   address = this.reference_address;
+  }
+
+  if (this.local_action_is_defined_at(address)){
+   return this.local_actions[address];
+  } else {
+   return undefined;
+  }
+ }
+
+ get_address_destinations(){
+  // returns an array of the addresses whose destinations are defined
+  return Object.keys(this.address_destinations);
+ }
+
+ destination_is_defined_at(address){
+  return (this.get_address_destinations().indexOf(address.toString())>-1?true:false);
+ }
+
  local_actions_to_address_destinations(){ // todo
   // only proceed if the address_destinations do not already exist
-  if (this.address_destinations == null){
+  if (this.address_destinations == undefined){
    console.log('convert reference address and local actions to address destinations');
   }
  }
 
  address_destinations_to_local_actions(){ // todo
   // only proceed if the reference_address and local_actions do not already exist
-  if (this.reference_address == null && Object.keys(this.local_actions).length == 0){
+  if (this.reference_address == undefined && Object.keys(this.local_actions).length == 0){
    console.log('convert address destinations to reference address and local actions');
   }
  }
 
+ destination_of_address(address){
+  var destination = undefined; // initialise
+  // is the address already in the automorphism's list of destinations?
+  // (this includes the reference address, if set with set_reference_address)
+  if (this.destination_is_defined_at(address)){
+   return this.address_destinations[address];
+  }
+
+  // otherwise, work along the path towards the reference address
+  var neighbour = path_from_to(address,this.reference_address)[1];
+  var neighbour_destination = this.destination_of_address(neighbour); // recursive step
+
+  // if the neighbour's destination could not be determined, we are done:
+  if (neighbour_destination == undefined){
+   return undefined;
+  }
+
+  // we now have the neighbour's destination, so apply its local action (might be the constant local action)
+  var neighbour_local_action = this.get_local_action_at_address(neighbour);
+
+  if (neighbour_local_action == undefined){
+   // no applicable local action, so the destination of this address is not defined
+   return undefined;
+  }
+
+  // which edge connects this address to the neighbour?
+  var e = get_edge(address,neighbour);
+  if (e == undefined){
+   // not actually a neighbour??
+   return undefined;
+  }
+
+  // almost there
+  destination = [];
+  for (var i=0;i<neighbour_destination.length;i++){
+   destination[i] = neighbour_destination[i]; // first, duplicate the destination
+  }
+  // and then add the permuted edge
+  destination.push(neighbour_local_action[e]);
+
+  // remove redundant (repeated) edges (eg. [0,1,2,2] -> [0,1])
+  destination = simplify_address(destination);
+
+  // store the destination for this address
+  this.address_destinations[address] = destination;
+
+  // and, finally, return the destination
+  return destination;
+
+ }
+ // xxx
+
+
 }
+
 
 // forest class
 //
@@ -321,4 +403,82 @@ class Forest {
   console.log('called remove_edge method for Forest');
  }
 
+}
+
+
+
+
+
+/*
+ Helper functions
+*/
+
+function path_from_to(from_address,to_address){
+ // returns the addresses along a path between the given addresses (inclusive on both ends)
+ var path = [];
+ var prefix = get_common_prefix(from_address,to_address); // common prefix for both addresses
+
+ // first, add the addresses from from_address back to the prefix address
+ for (var i=from_address.length;i>=prefix.length;i--){
+  var nextstep=[];
+  for (var j=0;j<i;j++){
+   nextstep.push(from_address[j]); // construct the next address in the path
+  }
+  path.push(nextstep); // put that address into the path list
+ }
+
+ // second, add the addresses from the prefix up to to_address
+ for (var i=prefix.length;i<to_address.length;i++){
+  var nextstep=[];
+  for (var j=0;j<=i;j++){
+   nextstep.push(to_address[j]); // construct the next address in the path
+  }
+  path.push(nextstep); // put that address into the path list
+ }
+ // finished
+ return path;
+}
+
+
+function get_common_prefix(address1,address2){
+ var prefix = [];
+ for (var i=0;i<Math.min(address1.length,address2.length);i++){
+  if (address1[i]==address2[i]){
+   prefix.push(address1[i]);
+  } else {
+   // stop
+   break;
+  }
+ }
+ return prefix;
+}
+
+
+function addresses_are_neighbours(address1,address2){
+ return (Math.abs(address1.length - address2.length) == 1);
+}
+
+
+function get_edge(address1,address2){
+ if (addresses_are_neighbours(address1,address2)){
+  // edge 'colour' is the difference between the neighbours:
+  if (address1.length>address2.length){
+   return address1[address1.length-1];
+  } else {
+   return address2[address2.length-1];
+  }
+ } else {
+  // not neighbours
+  return undefined;
+ }
+}
+
+
+function simplify_address(address){
+ for (var i=0;i<address.length-1;i++){
+  if (address[i]==address[i+1]){
+   address.splice(i,2); // remove both duplicated elements (ie. remove 2 elements starting at position i)
+  }
+ }
+ return address;
 }
